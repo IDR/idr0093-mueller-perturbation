aae814e2d3d6382312f4d4fdd1600690be74ffeb
Jean-Marie Burel <j.burel@dundee.ac.uk>
2021-01-26 21:33:23 +0000

    update namespace

--- scripts/add_notebook_links.py
+++ scripts/add_notebook_links.py
@@ -30,7 +30,7 @@ def add_annotation(conn, screen_id, delete_annotation):
     """
     notebook_name = "idr0093_expore.ipynb"
     ref_url = "https://mybinder.org/v2/gh/IDR/idr0093-mueller-perturbation/master?urlpath=notebooks%2Fnotebooks%2Fidr0093_expore.ipynb%3FplateId%3D"
-    namespace = "openmicroscopy.org/mapr/analysis/notebook"
+    namespace = "openmicroscopy.org/idr/analysis/notebook"
     for plate in conn.getObjects('Plate', opts={'screen': screen_id}):
         if delete_annotation:
             to_delete = []
